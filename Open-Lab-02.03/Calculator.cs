bbc5af8c01e01e307921f64c842b1e8937f36571
stark501 <mkudlacej@gmail.com>
2021-10-05 10:33:15 +0200

    Canculation

--- Open-Lab-02.03/Calculator.cs
+++ Open-Lab-02.03/Calculator.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_02._03
     {
         public int Remainder(int dividend, int divisor)
         {
-            throw new NotImplementedException();
+           int canculation;
+            canculation = dividend % divisor;
+            return canculation;
         }
     }
 }
